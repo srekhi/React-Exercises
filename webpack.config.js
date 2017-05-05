dedad300f5222a588dc56b79250ee14c33ab49bb
App Academy Student <student@appacademy.io>
2017-05-05 11:43:38 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./widgets.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+
+  module : {
+    loaders: [{
+      test: [/\.jsx?$/],
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+
+  }
+};
